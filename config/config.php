c70b969343aa2810820c77f8ddc63ddb59d25c24
Oscar Leche <oleche@donomar.local>
2015-08-27 12:03:34 -0600

    config fix

--- config/config.php
+++ config/config.php
@@ -13,12 +13,12 @@
 
 	$col_usuario = array('username', 'id_modulo', 'id_bot', 'created_at', 'updated_at');
 	$key_usuario = array('username');
-	$usuario = new DBManager($connection, 'usuario', $col_asset_type, $key_asset_type);
+	$usuario = new DBManager($connection, 'usuario', $col_usuario, $key_usuario);
 
-	$col_api_scopes = array('id_scope', 'id_client');
-	$key_api_scopes = array('id_scope', 'id_client');
-	$foreign_api_scopes = array('id_client' => array('api_users','client_id', $this->api_client), 'id_scope' => array('api_scopes','name', $this->scopes));
-	$this->api_client_scopes = new DBManager($this->connection, 'api_scope_users', $col_api_scopes, $key_api_scopes, $foreign_api_scopes);
-	
+	//$col_api_scopes = array('id_scope', 'id_client');
+	//$key_api_scopes = array('id_scope', 'id_client');
+	//$foreign_api_scopes = array('id_client' => array('api_users','client_id', $this->api_client), 'id_scope' => array('api_scopes','name', $this->scopes));
+	//``$this->api_client_scopes = new DBManager($connection, 'api_scope_users', $col_api_scopes, $key_api_scopes, $foreign_api_scopes);
+
 
 ?>
