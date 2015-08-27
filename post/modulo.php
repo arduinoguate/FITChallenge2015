b07531cd82afd107d5ab1deaab4a1e2991de52f6
Oscar Leche <oleche@donomar.local>
2015-08-27 15:54:10 -0600

    module select 8

--- post/modulo.php
+++ post/modulo.php
@@ -1,11 +1,11 @@
 <?php
 	include '../config/config.php';
-
+	echo $_POST['modulo'];
 	$usuario->columns['id_modulo'] = $_POST['modulo'];
 	if (!$usuario->update()){
 		header("location:control.php?error=WHAT");
 	}
 
-	header("location:../control.php");
+	//header("location:../control.php");
 
 ?>
