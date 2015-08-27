1e5d00ea8db4d676796e08896fc142dd9eb130f1
Oscar Leche <oleche@donomar.local>
2015-08-27 12:23:31 -0600

    control changes 3

--- control.php
+++ control.php
@@ -53,10 +53,14 @@
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="#" id="up" data-assigned="" data-value="" class="btn btn-info col-xs-12 col-sm-12 col-md-12 col-lg-12">Arriba</a></div>
         <br/>
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"><a href="#" id="lt" data-assigned="" data-value="" class="btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6">Izquierda</a></div>
-        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"><a href="#" id="rt" data-assigned="" data-value="" class="btn btn-info col-xs-6 col-sm-6 col-md-6 col-lg-6">Derecha</a></div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><a href="#" id="lt" data-assigned="" data-value="" class="btn btn-info col-xs-12 col-sm-12 col-md-12 col-lg-12">Izquierda</a></div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"><a href="#" id="rt" data-assigned="" data-value="" class="btn btn-info col-xs-12 col-sm-12 col-md-12 col-lg-12">Derecha</a></div>
         <br/>
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="#" id="dn" data-assigned="" data-value="" class="btn btn-info col-xs-12 col-sm-12 col-md-12 col-lg-12">Abajo</a></div>
+
+        <br/>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="#" id="st" data-assigned="" data-value="" class="btn btn-info col-xs-12 col-sm-12 col-md-12 col-lg-12">Detenerse</a></div>
       </div>
 
     </div><!-- /.container -->
