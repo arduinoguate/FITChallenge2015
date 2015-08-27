d4fd8ccbdf6261c4c3bad31c0f94a20183e99c88
Oscar Leche <oleche@donomar.local>
2015-08-27 15:57:20 -0600

    module select 9

--- post/modulo.php
+++ post/modulo.php
@@ -1,7 +1,10 @@
 <?php
+	session_start();
 	include '../config/config.php';
-	echo $_POST['modulo'];
+	
+	$usuario->fetch_id(array("username" => $_SESSION['username']));
 	$usuario->columns['id_modulo'] = $_POST['modulo'];
+	$usuario->columns['updated_at'] = date("Y-m-d H:i:s");
 	if (!$usuario->update()){
 		header("location:control.php?error=WHAT");
 	}
