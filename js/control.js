aec2d5489702be93f90cdc77803e1067f6f6786c
Oscar Leche <oleche@donomar.local>
2015-08-27 15:15:14 -0600

    add module 5

--- js/control.js
+++ js/control.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
       dataType: 'json',
       crossDomain: true,
       async: false,
+      dataType: 'jsonp',
       beforeSend: function(xhr) {
         xhr.setRequestHeader("Authorization", "Bearer " + token);
       },
