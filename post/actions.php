40b1f6d92806ca91b9a1a18d9bbae168952ccf98
Oscar Leche <oleche@donomar.local>
2015-08-27 18:27:43 -0600

    Configure 1

--- post/actions.php
+++ post/actions.php
@@ -2,10 +2,55 @@
 	session_start();
 	include '../config/config.php';
 
-	$usuario->fetch_id(array("username" => $_SESSION['username']));
-	$usuario->columns['id_modulo'] = $_POST['modulo'];
-	$usuario->columns['updated_at'] = date("Y-m-d H:i:s");
-	if (!$usuario->update()){
+	$q_list = array();
+	$q_list = $command->fetch("id_usuario = '$_SESSION['username']'");
+	if (count($q_list) > 0){
+		foreach ($q_list as $q_item) {
+			$q_item->->delete();
+		}
+	}
+
+	$command->columns['id'] = null;
+	$command->columns['id_action'] = $_POST['a_izquierda'];
+	$command->columns['default_value'] = $_POST['v_izquierda'];
+	$command->columns['updated_at'] = date("Y-m-d H:i:s");
+	$command->columns['id_usuario'] = $_SESSION['username'];
+	$command->columns['type'] = 'L';
+	$id = $command->insert();
+
+	$command->columns['id'] = null;
+	$command->columns['id_action'] = $_POST['a_derecha'];
+	$command->columns['default_value'] = $_POST['v_derecha'];
+	$command->columns['updated_at'] = date("Y-m-d H:i:s");
+	$command->columns['id_usuario'] = $_SESSION['username'];
+	$command->columns['type'] = 'R';
+	$id = $command->insert();
+
+	$command->columns['id'] = null;
+	$command->columns['id_action'] = $_POST['a_arriba'];
+	$command->columns['default_value'] = $_POST['v_arriba'];
+	$command->columns['updated_at'] = date("Y-m-d H:i:s");
+	$command->columns['id_usuario'] = $_SESSION['username'];
+	$command->columns['type'] = 'U';
+	$id = $command->insert();
+
+	$command->columns['id'] = null;
+	$command->columns['id_action'] = $_POST['a_izquierda'];
+	$command->columns['default_value'] = $_POST['v_izquierda'];
+	$command->columns['updated_at'] = date("Y-m-d H:i:s");
+	$command->columns['id_usuario'] = $_SESSION['username'];
+	$command->columns['type'] = 'D';
+	$id = $command->insert();
+
+	$command->columns['id'] = null;
+	$command->columns['id_action'] = $_POST['a_izquierda'];
+	$command->columns['default_value'] = $_POST['v_izquierda'];
+	$command->columns['updated_at'] = date("Y-m-d H:i:s");
+	$command->columns['id_usuario'] = $_SESSION['username'];
+	$command->columns['type'] = 'S';
+	$id = $command->insert();
+
+	if (!$command->update()){
 		header("location:control.php?error=WHAT");
 	}
 
